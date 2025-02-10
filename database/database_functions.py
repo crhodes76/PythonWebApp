031204b0c4d57cac0359800ed237d60efd787f67
Charles Rhodes <crhodes76@users.noreply.github.com>
2025-02-10 18:15:39 -0500

    established a database connection and performed a insert statement based on the data passed in

--- database/database_functions.py
+++ database/database_functions.py
@@ -1,20 +1,38 @@
 import pyodbc
+from datetime import datetime
 
-def insert_my_time_to_db(project_id, hours_worked, date, work_type):
-    conn = pyodbc.connect(
-        'DRIVER={SQL Server};'
-        'SERVER=localhost;'
-        'DATABASE=PythonDatabase;'
-        'UID=newuser;'  # Replace with your SQL Server username
-        'PWD=test1234'  # Replace with your SQL Server password
-        'Trusted_Connection=yes;'
-    )
-    cursor = conn.cursor()
+def insert_my_time_to_db(project_id, hours_worked, date, work_type):   
+    date_obj = datetime.strptime(date, '%Y-%m-%d')
+    connection = initiate_connection()
+    cursor = connection.cursor()
     cursor.execute("""
-        INSERT INTO MyTimeTracking (ProjectID, HoursWorked, Date, WorkType)
+        INSERT INTO [dbo].[MyTimeTracking] (date, hours, project_id, work_type)
         VALUES (?, ?, ?, ?)
-    """, project_id, hours_worked, date, work_type)
-    conn.commit()
+    """, date_obj, hours_worked, project_id, work_type)
+    connection.commit()
     cursor.close()
-    conn.close()
-    
+    close_connection(connection)
+
+def initiate_connection():
+    # Replace these values with your database details
+    server = 'localhost'
+    database = 'PythonDatabase'
+    username = 'power_user'
+    password = 'password1234'
+
+    # Connection string
+    connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+
+    try:
+        # Establish a connection
+        connection = pyodbc.connect(connection_string)
+        print("Connection successful!")
+        return connection
+
+    except pyodbc.Error as e:
+        print("Error: ", e)
+
+def close_connection(connection):
+    # Close the connection
+    if connection:
+        connection.close()
