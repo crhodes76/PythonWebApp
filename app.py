bc52b8c854c7dcb0c4a9891ace88290d4afbc705
Charles Rhodes <crhodes76@users.noreply.github.com>
2025-02-10 07:59:21 -0500

    Added model, get post request, property assignments

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template, request, jsonify
 from GeminiAiApiMethods import get_response, gemini_query_response
 from datetime import datetime
+from models.MyTimeModel import MyTimeModel
 app = Flask(__name__, template_folder='views')
 
 @app.route('/')
 def home():
     return render_template('/Home/index.html')
 
-@app.route('/my_time')
+@app.route('/my_time', methods=['GET', 'POST'])
 def my_time():
-    return render_template('/Home/my_time.html')
+    if request.method == 'POST':
+        project_id = request.form['project_id']
+        hours_worked = request.form['hours_worked']
+        mytime_model = MyTimeModel(project_id=project_id, hours_worked=hours_worked)
+        return render_template('/Home/my_time.html', mytime_model=mytime_model)
+    else:
+        return render_template('/Home/my_time.html', mytime_model=None)
 
 @app.route('/gemini_query', methods=['POST'])
 
---
+++ models/MyTimeModel.py
@@ -0,0 +1,4 @@
+class MyTimeModel:
+    def __init__(self, project_id, hours_worked):
+        self.project_id = project_id
+        self.hours_worked = hours_worked
